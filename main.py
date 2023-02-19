7a1096660de1b80f5de8b19ad6d93e1058fb474b
aarona2708 <aaronalejo2708@gmail.com>
2023-02-19 09:50:16 -0700

    Updated crime main and added dataframe functions.

--- main.py
+++ main.py
@@ -1,77 +1,50 @@
 import pandas as pd
-import plotly.graph_objects as go
+import json
 import plotly.express as px
+import plotly.graph_objects as go
 from dash import Dash, html, dcc, Input, Output
 
-#=================================================================================================================
-
-crime_file = "./source-files/Occurrences_Last_90_Days.csv"
-assessment_file = "./source-files/Property_Assessment_Data__Current_Calendar_Year_.csv"
-languages_file = "./source-files/2016_Census_-_Dwelling_Unit_by_Language__Neighbourhood_Ward_.csv"
-
-# create Pandas df and lists here
-df_crimes = pd.read_csv(crime_file)
-df_category = df_crimes.groupby(['Occurrence_Category'], as_index=False).size()
-
-df_languages = pd.read_csv(languages_file)
-column_header_lan = list(df_languages.columns.values)
-
-df_assessments = pd.read_csv(assessment_file)
-df_assessments = df_assessments[(df_assessments['Assessment Class 1'] == 'RESIDENTIAL')]
-neighbourhood_list = df_assessments['Neighbourhood'].unique()
-
-df_neighbourhood_average = df_assessments.groupby(['Neighbourhood'], as_index=False)[['Assessed Value']].mean().round(0)
-min_value = df_neighbourhood_average.min()[1]
-max_value = df_neighbourhood_average.max()[1]
-
-crimes_list = []
-
-for item in df_category.values.tolist():
-    crimes_list.append(item[0])
+# ==================================================================================================================== #
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+# ==================================================================================================================== #
+# MAIN PAGE DATAFRAMES AND DATA PREPROCESSING
+# ==================================================================================================================== #
+# CRIMES DATAFRAME
+df_crimes = pd.read_csv('./source-files/Occurrences_Last_90_Days.csv', low_memory=False)
+df_crimes_category = df_crimes.groupby(['Occurrence_Category'], as_index=False).size()
 
 
-#===================================================================================================================
+# df_crimes_property = df_crimes[(df_crimes['Occurrence_Group'] == 'Property')]
+# print(df_crimes_property)
 
-def create_graph_languages(value):
-    df = pd.read_csv(languages_file)
-    dff = df.loc[df['Neighbourhood Name'] == value]
-    headers = list(dff.columns.values)[4:14]
-    values = [
-        dff.iloc[0,  4], dff.iloc[0,  5], dff.iloc[0,  6], dff.iloc[0,  7],
-        dff.iloc[0,  8], dff.iloc[0,  9], dff.iloc[0, 10], dff.iloc[0, 11],
-        dff.iloc[0, 12], dff.iloc[0, 13],
-    ]
-    column_headers = headers[::-1]
-    value_list = values[::-1]
-    #print(f"{len(column_headers)} & {len(value_list)}")
+def occurence_group_controller(crime_stats):
+    new_df = df_crimes[df_crimes.Occurrence_Category == crime_stats]
 
-    fig = go.Figure(
-        data=[go.Bar(
-            x=value_list,
-            y=column_headers,
-            orientation="h",)
-        ],
-        layout=go.Layout(margin=dict(l=5, r=5, t=35, b=5)),
-    )
-    config = dict({'displayModeBar': False})
+    dfg = new_df.groupby('Occurrence_Group').count().reset_index()
 
-    return fig
+    return dfg["Occurrence_Group"].values.tolist()
 
-def create_average_assessment(value):
-    residential = "RESIDENTIAL"
-    df = pd.read_csv(assessment_file)
-    dff = df.query("`Neighbourhood`==@value & `Assessment Class % 1`==100 & `Assessment Class 1`==@residential")["Assessed Value"]
-    average = dff.mean()
-    currency_string = "${:,.2f}".format(average)
-    return currency_string
 
-#===============================================================================================
+def occurence_group_type_controller(occurence_group):
+    new_df = df_crimes[df_crimes.Occurrence_Group == occurence_group]
 
-# colours
-bg_assessedValue = "#1C6387"
-fg_assessedValue = "white"
+    dfg = new_df.groupby('Occurrence_Type_Group').count().reset_index()
 
-#===================================================================================================================
+    return dfg["Occurrence_Type_Group"].values.tolist()
 
-#APP LAYOUT
-app = Dash(__name__)
+
+def mapped_crime_list():
+    ret = {"Disorder": occurence_group_controller("Disorder"),
+           "Non-Violent": occurence_group_controller("Non-Violent"),
+           "Violent": occurence_group_controller("Violent"),
+           "Traffic": occurence_group_controller("Traffic"),
+           "Weapons": occurence_group_controller("Weapons"),
+           "Drugs": occurence_group_controller("Drugs"),
+           "Other": occurence_group_controller("Other")}
+
+    return ret
+
+crimes_list = df_crimes['Occurrence_Category'].unique()
+crimes_color_map = dict(zip(crimes_list, px.colors.qualitative.G10))
